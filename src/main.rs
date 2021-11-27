18941dce01e0465ffba97fb07d1615b4083924a8
Nico Borgsmüller <me@kaenganxt.dev>
2021-11-27 19:06:22 +0100

    WIP for FF 2021

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,187 @@
-
-use serde::{Serialize, Deserialize};
-use std::{time, thread, fs};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, rt::System};
+use serde::Serialize;
+use serde::Deserialize;
+use chrono::DateTime;
+use chrono::Utc;
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use std::io;
 
-#[derive(Serialize, Deserialize)]
+use std::fs;
+use std::fs::File;
+use actix_files::Files;
+use std::sync::RwLock;
+use std::io::Write;
+use chrono::Datelike;
+
+use std::collections::HashMap;
+use actix_web::middleware::Logger;
+use std::time;
+
+use simplelog::{WriteLogger, Config, LevelFilter};
+
+#[derive(Serialize)]
+enum ResponseState {
+    OK,
+    RELOAD,
+}
+
+#[derive(Serialize)]
+struct DataResponse<'a> {
+    state: ResponseState,
+    donations: &'a Vec<DonationState>,
+}
+
+#[derive(Deserialize)]
+struct DonationRequest {
+    last_request: Option<DateTime<Utc>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct DonationState {
     donations_count: u32,
     donor_count: u32,
     donated_amount_in_cents: u64,
-    updated_at: String
+    updated_at: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Donations {
-    donations: Vec<DonationState>
+    donations: Vec<DonationState>,
 }
 
-fn main() {
+struct AppState {
+    donations: RwLock<HashMap<i32, Donations>>,
+    static_updated: RwLock<DateTime<Utc>>,
+}
 
-    let (tx, rx): (Sender<DonationState>, Receiver<DonationState>) = mpsc::channel();
+#[get("/years")]
+async fn year_list(state: web::Data<AppState>) -> impl Responder {
+    let years = state.donations.read().unwrap();
+    let years = years.keys().collect::<Vec<&i32>>();
+    HttpResponse::Ok().json(years)
+}
 
-    thread::spawn(move || {
+#[get("/donations/{year}")]
+async fn data(data: web::Data<AppState>, info: web::Query<DonationRequest>, web::Path(year): web::Path<i32>) -> impl Responder {
+    let last_static_update = data.static_updated.read().unwrap();
+    let state = if info.last_request.is_some() && info.last_request.unwrap() < *last_static_update {
+        ResponseState::RELOAD
+    } else {
+        ResponseState::OK
+    };
+    let donations = data.donations.read().unwrap();
+    if !donations.contains_key(&year) {
+        HttpResponse::BadRequest().body(format!("Year {} is not supported", year))
+    } else {
+        HttpResponse::Ok().json(DataResponse { state, donations: &donations.get(&year).unwrap().donations })
+    }
+}
 
-        loop {
-            let state = rx.recv();
-            match state {
-                Ok(donation) => {
-                    let file = fs::File::open("donations.json").expect("file should open read only");
-
-                    let mut donations: Donations = serde_json::from_reader(file).expect("JSON was not well-formatted");
-                    donations.donations.push(donation);
-                    let res = serde_json::to_string_pretty(&donations).unwrap();
-                    fs::write("donations.json", res).unwrap();
-                },
-                Err(_) => {}
-            }
-        }
-    });
-
+fn request_thread(donations_store: web::Data<AppState>) {
+    let begin = time::Instant::now();
+    let this_year = Utc::now().year();
     loop {
         let now = time::Instant::now();
         let body = reqwest::blocking::get("https://api.betterplace.org/de/api_v4/fundraising_events/36081.json").unwrap();
         let res : DonationState = body.json().unwrap();
 
-        tx.send(res).unwrap();
+        // Critical section, will block all donation HTTP requests
+        {
+            let mut writer = donations_store.donations.write().unwrap();
+            let this_year_data = writer.get_mut(&this_year);
+            if this_year_data.is_none() {
+                panic!("No data structure for this year available! Maybe this program ran into the new year?");
+            }
+
+            //this_year_data.unwrap().donations.push(res);
+            this_year_data.unwrap().donations.push(DonationState { donations_count: 10, donor_count: 10, donated_amount_in_cents: begin.elapsed().as_secs() * 1000, updated_at: Utc::now().to_rfc2822()});
+        }
+
+        let res = serde_json::to_string_pretty(&donations_store.donations.read().unwrap().get(&this_year)).unwrap();
+        fs::write(format!("donations_{}.json", this_year), res).unwrap();
 
         let one_minute = time::Duration::from_secs(60);
         thread::sleep(one_minute - now.elapsed());
     }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    WriteLogger::init(LevelFilter::Info, Config::default(), File::create("donations_web.log").unwrap()).unwrap();
+
+    let this_year = Utc::now().year();
+
+    let mut years = HashMap::new();
+    for year in 2020..=this_year {
+        let file = File::open(format!("donations_{}.json", year));
+        if file.is_ok() {
+            let donations_read = serde_json::from_reader(file.unwrap()).expect("JSON was not well-formatted (Reading input file)");
+            years.insert(year, donations_read);
+        }
+    }
+
+    println!("Loaded supported years: {:?}", years.keys());
+
+    if !years.contains_key(&this_year) {
+        years.insert(this_year, Donations { donations: vec![] });
+    }
+
+    let static_updated = RwLock::new(Utc::now());
+    let donations: web::Data<AppState> = web::Data::new(AppState { donations: RwLock::new(years), static_updated });
+
+    let donations_copy = donations.clone();
+
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let sys = System::new("http-server");
+
+        let srv = HttpServer::new(move || {
+            App::new()
+                .app_data(donations_copy.clone())
+                .wrap(Logger::new("%r -> %s (took %Ts)"))
+                .service(web::scope("/api").service(data).service(year_list))
+                .service(Files::new("/", "static").index_file("index.html"))
+        })
+        .bind("0.0.0.0:8080")?
+        .shutdown_timeout(30)
+        .run();
+
+        let _ = tx.send(srv);
+        sys.run()
+    });
+
+    let donations_copy = donations.clone();
+    thread::spawn(move || request_thread(donations_copy));
+
+    let srv = rx.recv().unwrap();
+
+    println!("Server started. Starting CLI.");
+    let mut line;
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        line = String::new();
+        let res = io::stdin().read_line(&mut line);
+        if !res.is_ok() {
+            println!("Invalid input");
+            continue;
+        } else if res.unwrap() == 0 {
+            println!("EOF, stopping...");
+            srv.stop(true).await;
+            break;
+        } else if line == "stop\n" || line == "exit\n" || line == "quit\n" || line == "q\n" {
+            println!("Stopping...");
+            srv.stop(true).await;
+            break;
+        } else if line == "reload\n" {
+            println!("Queueing reload...");
+            let mut writer = donations.static_updated.write().unwrap();
+            *writer = Utc::now();
+        } else {
+            println!("Unknown command. Known commands include reload and stop.");
+        }
+    }
+    Ok(())
 }
---
+++ static/script.js
@@ -0,0 +1,294 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    getSupportedYears();
+    updateData();
+    createChart();
+    setInterval(updateData, 1000 * 60 * 2);
+});
+
+let lastRequest = new Date();
+let currentYear = lastRequest.getFullYear();
+let yearsLoaded = [currentYear];
+let data = {};
+let chart;
+
+function createChart() {
+    ajax('https://cdn.jsdelivr.net/npm/apexcharts/dist/locales/de.json').then(function(de) {
+        chart.w.config.chart.defaultLocale = "de";
+        chart.w.config.chart.locales = [de];
+        chart.setLocale("de");
+        chart.render();
+    });
+    let options = {
+        series: [],
+        chart: {
+            id: 'realtime',
+            height: 350,
+            type: 'line',
+            animations: {
+                enabled: true,
+                easing: 'easein',
+                dynamicAnimation: {
+                    speed: 500
+                }
+            },
+            toolbar: {
+                show: true
+            },
+            zoom: {
+                enabled: true
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        title: {
+            text: 'Friendly Fire Spendenfortschritt',
+            align: 'left'
+        },
+        markers: {
+            size: 0
+        },
+        xaxis: {
+            type: 'datetime'
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value) {
+                    if (!value) {
+                        return value + " €";
+                    }
+                    if (value >= 1000000) {
+                        return (value/1000000).toFixed(2) + " Mio €";
+                    } else if (value >= 1000) {
+                        return (value/1000).toFixed(1) + " Tsd €";
+                    }
+                    return value.toFixed(0) + " €";
+                }
+            }
+        },
+        legend: {
+            show: false
+        },
+        grid: {
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            }
+        },
+        tooltip: {
+            x: {
+                format: "HH:mm"
+            }
+        },
+        theme: {
+            mode: "light",
+            monochrome: {
+                enabled: true,
+                color: "#faa422",
+
+            }
+        },
+        annotations: {
+            yaxis: [
+            {
+                y: 170000,
+                borderColor: '#D27E05',
+                label: {
+                    borderColor: '#D27E05',
+                    style: {
+                        color: '#fff',
+                        background: '#D27E05'
+                    },
+                    text: 'Friendly Fire 1',
+                    offsetY: 15
+                }
+            },
+            {
+                y: 200000,
+                borderColor: '#D27E05',
+                label: {
+                borderColor: '#D27E05',
+                style: {
+                    color: '#fff',
+                    background: '#D27E05'
+                },
+                text: 'Friendly Fire 2'
+                }
+            },
+            {
+                y: 470000,
+                borderColor: '#D27E05',
+                label: {
+                borderColor: '#D27E05',
+                style: {
+                    color: '#fff',
+                    background: '#D27E05'
+                },
+                text: 'Friendly Fire 3'
+                }
+            },
+            {
+                y: 620000,
+                borderColor: '#D27E05',
+                label: {
+                borderColor: '#D27E05',
+                style: {
+                    color: '#fff',
+                    background: '#D27E05'
+                },
+                text: 'Friendly Fire 4'
+                }
+            },
+            {
+                y: 730000,
+                borderColor: '#D27E05',
+                label: {
+                borderColor: '#D27E05',
+                style: {
+                    color: '#fff',
+                    background: '#D27E05'
+                },
+                text: 'Friendly Fire 5'
+                }
+            },
+            {
+                y: 1052548,
+                borderColor: '#fab142',
+                label: {
+                borderColor: '#fab142',
+                style: {
+                    color: '#fff',
+                    background: '#fab142'
+                },
+                text: 'Friendly Fire 6'
+                }
+            }
+            ]
+        }
+    };
+
+    chart = new ApexCharts(document.querySelector("#donations"), options);
+    chart.render();
+}
+
+function updateChart(year) {
+    let render_data = [];
+    for (let date of data[year]) {
+        let date_date = new Date(date.updated_at);
+        date_date.setFullYear(2000);
+        date_date.setMonth(1);
+        if (date_date.getHours() < 12) {
+            date_date.setDate(2);
+        } else {
+            date_date.setDate(1);
+        }
+        render_data.push([date_date, date.donated_amount_in_cents / 100]);
+    }
+
+    let series = chart.w.config.series;
+    let newData = [];
+    for (let data of series) {
+        if (data.name != year) {
+            newData.push(data);
+        }
+    }
+    newData.push({
+        name: year,
+        data: render_data
+    });
+
+    chart.updateSeries(newData);
+}
+
+function setSeriesVisibility(year, visible) {
+    if (yearsLoaded.indexOf(year) == -1) {
+        if (visible) {
+            updateData(year);
+        }
+        return;
+    }
+    if (visible) {
+        chart.showSeries(year);
+    } else {
+        chart.hideSeries(year);
+    }
+}
+
+function updateData(year) {
+    if (typeof year === "undefined") {
+        year = currentYear;
+    }
+    let query = "";
+    if (currentYear == year) {
+        query = "?last_request=" + encodeURIComponent(lastRequest.toISOString());
+        lastRequest = new Date();
+    }
+
+    ajax("/api/donations/" + year + query).then(function(response) {
+        if (response.state === "RELOAD") {
+            window.location.reload();
+        } else {
+            data[year] = response.donations;
+            updateChart(year);
+            if (yearsLoaded.indexOf(year) == -1) {
+                yearsLoaded.push(year);
+            }
+        }
+    });
+}
+
+function getSupportedYears() {
+    ajax("/api/years").then(function(years) {
+        let year_elems = document.getElementById("years");
+        year_elems.innerText = "";
+        for (let year of years) {
+            let new_elem = document.createElement("li");
+            new_elem.classList.add("yearSelect");
+            new_elem.innerText = year;
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.onchange = function() {
+                setSeriesVisibility(year, checkbox.checked);
+            };
+            if (year == currentYear) {
+                checkbox.checked = "checked";
+            }
+            new_elem.prepend(checkbox),
+
+            year_elems.append(new_elem);
+        }
+    });
+}
+
+function ajax(url, method = "GET", data = "", tryJson = true) {
+    return new Promise((resolve, reject) => {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    var response = httpRequest.responseText;
+                    if (tryJson) {
+                        try {
+                            response = JSON.parse(response);
+                        } catch (e) {}
+                    }
+                    resolve(response);
+                } else {
+                    reject();
+                }
+            }
+        };
+        httpRequest.open(method, url, true);
+        if (method === "POST") {
+            httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            httpRequest.send(data);
+        } else {
+            httpRequest.send();
+        }
+    });
+}
